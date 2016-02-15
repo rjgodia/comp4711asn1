65acbee121450c2ce881a38d74752a9e60885c1e
Rawrisaur <n.tan@hotmail.ca>
2016-02-15 14:24:23 -0800

    Added beta dice roll function

--- application/views/play.php
+++ application/views/play.php
@@ -1,1 +1,17 @@
-Put game play stuff here dhgiaoga niggah
+<script>	
+	function generate()
+	{
+			var x = document.getElementById("number");
+			x.innerHTML=Math.floor((Math.random()*6+1));
+	}
+	
+	function 
+	
+</script>
+
+Put game play stuff here
+</br>
+
+<button onclick="generate()" type="button" name="buttonpassvalue" id="random">ROLL DIE</button>
+
+<div id="number"></div>//<--THIS IS WHERE YOUR ROLL IS PLACED
