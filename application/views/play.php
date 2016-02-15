9a2e107861adb99e2a968fd666ec2f9851477d83
kaniseo <dannyseo517@gmail.com>
2016-02-15 15:15:33 -0800

    fixed dice

--- application/views/play.php
+++ application/views/play.php
@@ -1,6 +1,8 @@
 <?php
 	$dice1 = array("up", "down", "div");
-	echo $dice2[array_rand($a)];
+	echo $dice1[array_rand($dice1)];
+	
+	echo "</br>";
 	
 	$dice2 = array(5,10,20);
 	echo $dice2[array_rand($dice2)];
