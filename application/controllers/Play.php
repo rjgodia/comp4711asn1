977cc5ef66e3e9b2e086df073499c1cd40db4975
rjgodia <ahrjhey.godia@yahoo.com>
2016-02-15 20:14:15 -0800

    created stock MVC

--- application/controllers/Play.php
+++ application/controllers/Play.php
@@ -1,19 +1,21 @@
 <?php
-	defined('BASEPATH') OR exit('No direct script access allowed');
+    defined('BASEPATH') OR exit('No direct script access allowed');
 	
 	
-	class Play extends Application {
-		
-		//constructor
-		function __construct() 
-		{
-        	parent::__construct();
-    	}
-		
-		public function index()
-		{
-			$this->data['pagebody'] = 'play';
-			$this->render();
-		}
-	}
-?>
+    class Play extends Application {
+
+        //constructor
+        function __construct() 
+        {
+            parent::__construct();
+        }
+
+        function index()
+        {
+            $this->data['pagebody'] = 'play';
+            //$this->data['test'] = var_dump($this->Stocks->all());
+            $this->data['test'] = $this->Stocks->all();
+            
+            $this->render();
+        }
+    }
