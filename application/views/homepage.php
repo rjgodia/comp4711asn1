640c93199e3f1235504854e3278eefa635c8d34b
rjgodia <ahrjhey.godia@yahoo.com>
2016-02-16 18:04:37 -0800

    updated homepage

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,25 +1,35 @@
-</br>
-<img class="center-block" src="/assets/images/gameimage.jpg"/>
-</br>
-
-<h4>Stock Ticker</h4>
-StockTicker is a game which simulates buying and selling stocks in a stock
-market. An indeterminate number of stocks will see their per share value rise and
-fall, under the control of a game engine, the BCIT Stock Exchange (BSX). Each
-team's webapp represents a stock brokerage active on the BSX. The object of
-the game is for players to build as much equity (stocks and cash) as possible.
-
-</br>
-
-<h4>Stock Ticker Game Play</h4>
-Movements are generated periodically, based on stock categories.
-Each movement will have a randomly chosen stock of that category, a randomly
-chosen action (up, down or div), and a random amount (5, 10 or 20).
-If a stock price falls below 0, it is “delisted”. Any holdings in that stock are forfeit,
-and the stock price reverts to 100.
-If a stock price rises above 200, it is “split”. Any holdings in that stock are
-doubled, and the stock price is reset to 100.
-If a stock action is a dividend (“div”), said dividend is only paid if the stock is
-trading at or above par at that point.
-The unit of currency is peanuts.
-The next iteration of the game (assignment 2) will incorporate stock certificates.
+<div class = "row">
+    <div class="span3">
+        <table class="table table-hover">
+            <tr>
+                <th>Stock</th>
+                <th>Value</th>
+            </tr>
+            {stock_list}
+            <tr>
+                <td>{Code}</td>
+                <td>{Value}</td>
+            </tr>
+            {/stock_list}
+        </table>
+    </div>
+    
+    <div class="span9">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Player</th>
+                    <th>Cash</th>
+                    <th>Equity<th>
+                </tr>
+            </thead>
+            {player_list}
+            <tr>
+                <td><a href="#">{Player}</a></td>
+                <td>{Cash}</td>
+                <td>{Equity}</td>
+            </tr>
+            {/player_list}
+        </table>
+    </div>
+</div>
