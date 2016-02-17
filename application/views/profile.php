51387f0c44046d5cd62df67224431ea94aa041f8
Rawrisaur <n.tan@hotmail.ca>
2016-02-16 16:13:09 -0800

    WIP Profile Page

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,1 +1,33 @@
-this is the user's profile huehue
+<?php
+    function view(){
+        echo "TEST";
+    }
+?>
+<div>
+
+    <form>
+        Select the player who's portfolio you would like to view:
+        </br>
+        <select name="players">
+            {players}
+            <option value={Player}>{Player}</option>
+            {/players}
+        </select>
+        </br>
+        <input onclick="view()" type="button" value="View">
+    </form>
+    </br></br>
+    <h1>Trading Activity</h1>
+    <table style="width: 25%">
+        <tr>
+            <td>Purchases/Sales:</td>
+            <td>Quantity:</td>
+        </tr>
+        {history}
+        <tr>
+            <td>{Trans}</td>
+            <td>{Quantity}</td>
+        </tr>
+        {/history}
+    </table>
+</div>
