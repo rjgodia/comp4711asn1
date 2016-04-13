643847527265842661c51fbc560d07a3b62cf5fd
kaniseo <dannyseo517@gmail.com>
2016-04-12 18:43:31 -0700

    added fopen

--- application/models/Stocks.php
+++ application/models/Stocks.php
@@ -18,4 +18,42 @@ class Stocks extends MY_Model
     {
         parent:: __construct('Stocks', 'Value');
     }
+	
+
+	
+	function recent_stock($filename)
+	{
+		$delimiter = ",";
+		
+	
+		$header = NULL;
+		$data = array();
+		if (($handle = fopen($filename, 'r')) !== FALSE)
+		{
+			while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+			{
+				if(!$header)
+					$header = $row;
+				else
+					$data[] = array_combine($header, $row);
+			}
+			fclose($handle);
+		}
+		return $data;
+	}
+	function test(){
+	
+		// create a new cURL resource
+		$ch = curl_init();
+		
+		// set URL and other appropriate options
+		curl_setopt($ch, CURLOPT_URL, "http://bsx.jlparry.com/data/movement");
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		
+		// grab URL and pass it to the browser
+		return $this->csv_to_array(curl_exec($ch));
+		
+		// close cURL resource, and free up system resources
+		curl_close($ch);
+	}
 }
